3744a8647bc2f87c143c1c0ed0c11a9482ff3beb
emmaaanu <82918561+emmaaanu@users.noreply.github.com>
2021-09-13 20:49:25 -0300

    otro commit con un cambio

--- index.ts
+++ index.ts
@@ -24,7 +24,7 @@ function main() {
     const unaBanda = new Banda(["Emmanuel"],[unAlbum, unAlbum, unAlbum]);
     console.log(unaBanda.members);
     console.log(unaBanda.albums);
-    console.log("otro cambio");
+    console.log("vino otra persona y le metio este cambio");
 }
 
 main();
